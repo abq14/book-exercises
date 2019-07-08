252b60687eda8fe3ab3c71519f75aad456c7fc39
abq14 <abigailquinsay@gmail.com>
2019-07-08 08:17:36 -0700

    Complete some exercises

--- chapter-10-exercises/exercise-3/exercise.R
+++ chapter-10-exercises/exercise-3/exercise.R
@@ -2,17 +2,17 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+UPE <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
 
--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -1,39 +1,45 @@
 # Exercise 4: external data sets: Gates Foundation Educational Grants
 
-# Use the `read.csv()` functoin to read the data from the `data/gates_money.csv`
+# Use the `read.csv()` function to read the data from the `data/gates_money.csv`
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+grants <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- grants$organization
+organization
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean_grant <- mean(grants$total_amount)
+mean_grant
 
 # What was the dollar amount of the largest grant?
-
+largest_grant <- max(grants$total_amount)
+largest_grant
 
 # What was the dollar amount of the smallest grant?
-
+smallest_grant <- min(grants$total_amount)
+smallest_grant
 
 # Which organization received the largest grant?
-
+largest_grant_receive <- organization[grants$total_amount == largest_grant]
+largest_grant_receive
 
 # Which organization received the smallest grant?
-
+smallest_grant_receive <- organization[grants$total_amount == smallest_grant]
+smallest_grant_receive
 
 # How many grants were awarded in 2010?
-
+length(grants$total_amount[grants$start_year == 2010])
